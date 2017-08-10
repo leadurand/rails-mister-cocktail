8dadbbda15b42935606ef6d029e10ede2d4b019c
Lea Durand <leadurand.pro@outlook.com>
2017-08-10 18:45:31 +0200

    created views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint = Ingredient.create!(name: "mint leaves")
+
+russian = Cocktail.create!(name: "White Russian")
+mojito = Cocktail.create!(name: "Mojito")
+sex = Cocktail.create!(name: "Sex on the Beach")
+
+first = Dose.create!(description: "5cl White Vodka", cocktail: russian, ingredient: ice)
+second = Dose.create!(description: "8cl Rhum", cocktail: mojito, ingredient: mint)
+third = Dose.create!(description: "4cl Vodka",cocktail: sex, ingredient: lemon )
