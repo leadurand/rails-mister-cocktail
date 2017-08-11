abc07e828f8fadbda2dc066189b850d35e8f50b0
Lea Durand <leadurand.pro@outlook.com>
2017-08-11 15:07:50 +0200

    added seed for production

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -10,7 +10,8 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@dose.cocktail)
     else
-      render :new
+      @cocktail = @dose.cocktail
+      render "cocktails/show"
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,58 @@
+# Dose.destroy_all
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+
+# lemon = Ingredient.create!(name: "lemon")
+# ice = Ingredient.create!(name: "ice")
+# mint = Ingredient.create!(name: "mint leaves")
+
+# russian = Cocktail.create!(name: "White Russian")
+# mojito = Cocktail.create!(name: "Mojito")
+# sex = Cocktail.create!(name: "Sex on the Beach")
+
+# first = Dose.create!(description: "5cl White Vodka", cocktail: russian, ingredient: ice)
+# second = Dose.create!(description: "8cl Rhum", cocktail: mojito, ingredient: mint)
+# third = Dose.create!(description: "4cl Vodka",cocktail: sex, ingredient: lemon )
+
+# puts 'do you want to add cocktail ?'
+
+
 Dose.destroy_all
-Ingredient.destroy_all
 Cocktail.destroy_all
+Ingredient.destroy_all
 
-lemon = Ingredient.create!(name: "lemon")
-ice = Ingredient.create!(name: "ice")
-mint = Ingredient.create!(name: "mint leaves")
 
-russian = Cocktail.create!(name: "White Russian")
-mojito = Cocktail.create!(name: "Mojito")
-sex = Cocktail.create!(name: "Sex on the Beach")
+cocktails = [
+{
+  name: "Old Fashionned"
+},
+{
+  name: "Daiquiri"
+},
+{
+  name: "Margarita"
+},
+{
+  name: "French 75"
+},
+{
+  name: "Tom Collins"
+}
+]
+v
+puts "Seed ingredient? (Y/N)"
 
-first = Dose.create!(description: "5cl White Vodka", cocktail: russian, ingredient: ice)
-second = Dose.create!(description: "8cl Rhum", cocktail: mojito, ingredient: mint)
-third = Dose.create!(description: "4cl Vodka",cocktail: sex, ingredient: lemon )
+answer = STDIN.gets.chomp.upcase
+
+if answer != "N"
+  ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+  ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
+end
+
+puts "Seed cocktails? (Y/N)"
+
+answer = STDIN.gets.chomp.upcase
+
+if answer != "N"
+  cocktails.each { |cocktail| Cocktail.create(cocktail) }
+end
